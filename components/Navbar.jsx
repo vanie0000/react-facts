28e364502558105cf488569eb2fd6c4c1e99fe9f
shraavanijha <124703685+vanie0000@users.noreply.github.com>
2025-04-12 01:48:46 +0530

    initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <img src="../images.png" alt="react logo"/>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Main from "../components/Main.jsx"
+import Navbar from "../components/Navbar.jsx"
+export default function App() {
+  return (
+    <>
+    <Navbar/>
+    <Main/>
+    </>
+  )
+}
